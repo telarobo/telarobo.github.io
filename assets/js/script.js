f257917a4211b5f92163fada2ae5906994df0478
Telarobo <99219964+telarobo@users.noreply.github.com>
2022-02-11 17:19:03 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
-$(document).ready(function(){
-	$('.frame').click(function(){
-		$('.top').addClass('open');
-		$('.message').addClass('pull');
-	})
-});
+// Example 1: From an element in DOM
+$('.open-popup-link').magnificPopup({
+	type:'inline',
+	midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+  
+  // Example: 2 Dynamically created
+  $('button').magnificPopup({
+	items: {
+		src: '<div class="white-popup">Dynamically created popup</div>',
+		type: 'inline'
+	},
+	closeBtnInside: true
+  });
+  
+  
