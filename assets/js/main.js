80b5122b4542be6e301d70ab8362601698f6bbb2
Telarobo <99219964+telarobo@users.noreply.github.com>
2022-02-11 15:27:14 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,10 +20,10 @@
 
     if (window.pageYOffset > sticky) {
       header_navbar.classList.add("sticky");
-      logo.src = "assets/img/logo/logo.png";
+      logo.src = "assets/img/logo/logo-custom.png";
     } else {
       header_navbar.classList.remove("sticky");
-      logo.src = "assets/img/logo/logo.png";
+      logo.src = "assets/img/logo/logo-custom.png";
     }
 
     // show or hide the back-top-top button
